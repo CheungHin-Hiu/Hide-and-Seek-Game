88e1aee6b6306aab0b398eeba97c80cacb205fc7
Toby-Cheung <tobycheung227@gmail.com>
2023-11-21 01:45:58 +0800

    auto maze gen completed

--- src/main/java/MazeGenerator.java
+++ src/main/java/MazeGenerator.java
@@ -1,108 +1,151 @@
-import java.util.ArrayList;
-import java.util.Stack;
-import java.util.Random;
-import java.util.Arrays;
+import java.util.*;
 
-class MazeGenerator {
-
-    private Stack<Position> stack = new Stack<>();
-    private Random rand = new Random();
+public class MazeGenerator {
     private int[][] maze;
     private int dimension;
+    private List<Wall> walls;
+    private Random rand = new Random();
+    private Position start;
+    private Position end;
 
-    MazeGenerator(int dim) {
-        maze = new int[dim][dim];
+    // Represents a wall between cells, with direction to the neighboring cell
+    private class Wall {
+        int x, y; // Coordinates of the wall (x for column, y for row)
+        int dx, dy; // Direction from the wall to the cell it might connect to
+
+        Wall(int x, int y, int dx, int dy) {
+            this.x = x;
+            this.y = y;
+            this.dx = dx;
+            this.dy = dy;
+        }
+
+        // Checks if the wall leads to a valid unvisited cell (not including maze boarder)
+        boolean isValid() {
+            int nx = x + dx;
+            int ny = y + dy;
+            return nx > 0 && nx < dimension - 1 && ny > 0 && ny < dimension - 1 && maze[ny][nx] == 1;
+        }
+    }
+
+    public MazeGenerator(int dim) {
         dimension = dim;
+        maze = new int[dim][dim];
+        walls = new ArrayList<>();
+        // Randomize start and end positions within the maze bounds
+        start = new Position(0, rand.nextInt(dim - 2) + 1);
+        end = new Position(0, 0);
+        initializeMaze();
     }
 
-    public void generateMaze() {
-        stack.push(new Position(0,0));
-        while (!stack.empty()) {
-            Position next = stack.pop();
-            if (validNextNode(next)) {
-                maze[next.y][next.x] = 1;
-                ArrayList<Position> neighbors = findNeighbors(next);
-                randomlyAddNodesToStack(neighbors);
-            }
-        }
-    }
-
-    public String getRawMaze() {
-        StringBuilder sb = new StringBuilder();
-        for (int[] row : maze) {
-            sb.append(Arrays.toString(row) + "\n");
-        }
-        return sb.toString();
-    }
-
-
-    public String getSymbolicMaze() {
-        StringBuilder sb = new StringBuilder();
+    // Initializes the maze with walls
+    private void initializeMaze() {
         for (int i = 0; i < dimension; i++) {
-            for (int j = 0; j < dimension; j++) {
-                sb.append(maze[i][j] == 1 ? "*" : " ");
-                sb.append("  ");
-            }
-            sb.append("\n");
+            Arrays.fill(maze[i], 1); // 1 represents a wall
         }
-        return sb.toString();
     }
 
+    // Generates the maze using Prim's algorithm
+    public void generateMaze() {
+        addWalls(start.x, start.y);
 
+        while (!walls.isEmpty()) {
+            Wall wall = walls.remove(rand.nextInt(walls.size()));
+            if (wall.isValid()) {
+                maze[wall.y][wall.x] = 0; // 0 represents a path
+                addWalls(wall.x + wall.dx, wall.y + wall.dy);
+            }
+        }
+        updateEndPoint();
+    }
 
-    private boolean validNextNode(Position position) {
-        int numNeighboringOnes = 0;
-        for (int y = position.y-1; y < position.y+2; y++) {
-            for (int x = position.x-1; x < position.x+2; x++) {
-                if (pointOnGrid(x, y) && pointNotNode(position, x, y) && maze[y][x] == 1) {
-                    numNeighboringOnes++;
+    // Adds walls around a given cell to the list of walls
+    private void addWalls(int x, int y) {
+        maze[y][x] = 0; // Mark cell as part of the maze
+
+        // all checking do not include maze boarder
+        if (x > 0) walls.add(new Wall(x - 1, y, -1, 0)); // Left wall
+        if (y > 0) walls.add(new Wall(x, y - 1, 0, -1)); // Top wall
+        if (x < dimension - 1) walls.add(new Wall(x + 1, y, 1, 0)); // Right wall
+        if (y < dimension - 1) walls.add(new Wall(x, y + 1, 0, 1)); // Bottom wall
+    }
+
+    private void updateEndPoint(){
+        List<Position> possibleEndPointPositions = new ArrayList<>();
+        for (int y = 1; y < dimension - 1; y++){
+            if (isStraightWall(dimension - 1, y)){
+                possibleEndPointPositions.add(new Position(dimension - 1, y));
+            }
+        }
+        Collections.shuffle(possibleEndPointPositions); // Randomize the list
+        Position pos = possibleEndPointPositions.get(0);
+        maze[pos.y][pos.x] = 0; // Turn the wall into a path
+        end.x = pos.x; //update end point
+        end.y = pos.y;
+    }
+
+    // Computes the sum of walls surrounding a given cell
+    private int getWeightedSurroundingVertexSum(int x, int y) {
+        int sum = 0;
+        if (x > 0 && maze[y][x - 1] == 1) sum += 3; // Check left
+        if (x < dimension - 1 && maze[y][x + 1] == 1) sum += 3; // Check right
+        if (y > 0 && maze[y - 1][x] == 1) sum += 2; // Check above
+        if (y < dimension - 1 && maze[y + 1][x] == 1) sum += 2; // Check below
+        return sum;
+    }
+
+    private boolean isStraightWall(int x, int y) {
+        int sum = getWeightedSurroundingVertexSum(x, y);
+        return maze[y][x] == 1 && (sum == 6 || sum == 4);
+    }
+
+    public void addPaths(int numWallsToBreak) {
+        List<Position> breakableWallPositions = new ArrayList<>();
+        for (int y = 1; y < dimension - 1; y++) {
+            for (int x = 1; x < dimension - 1; x++) {
+                if (isStraightWall(x, y)){
+                    breakableWallPositions.add(new Position(x, y));
                 }
             }
         }
-        return (numNeighboringOnes < 3) && maze[position.y][position.x] != 1;
-    }
 
-    private void randomlyAddNodesToStack(ArrayList<Position> positions) {
-        int targetIndex;
-        while (!positions.isEmpty()) {
-            targetIndex = rand.nextInt(positions.size());
-            stack.push(positions.remove(targetIndex));
+        Collections.shuffle(breakableWallPositions); // Randomize the list of breakable wall positions
+        for (int i = 0; i < Math.min(numWallsToBreak, breakableWallPositions.size()); i++) {
+            Position pos = breakableWallPositions.get(i);
+            maze[pos.y][pos.x] = 0; // Turn the wall into a path
         }
     }
 
-    private ArrayList<Position> findNeighbors(Position position) {
-        ArrayList<Position> neighbors = new ArrayList<>();
-        for (int y = position.y-1; y < position.y+2; y++) {
-            for (int x = position.x-1; x < position.x+2; x++) {
-                if (pointOnGrid(x, y) && pointNotCorner(position, x, y)
-                        && pointNotNode(position, x, y)) {
-                    neighbors.add(new Position(x, y));
+    public void printBreakableWallMaze() {
+        for (int y = 0; y < dimension; y++) {
+            for (int x = 0; x < dimension; x++) {
+                if (maze[y][x] == 1) { // It's a wall
+                    System.out.print(isStraightWall(x,y) ? " 2 " : " " + maze[y][x] + " ");
+                }else{
+                    System.out.print("   ");
                 }
             }
+            System.out.println();
         }
-        return neighbors;
     }
 
-    private Boolean pointOnGrid(int x, int y) {
-        return x >= 0 && y >= 0 && x < dimension && y < dimension;
+    // Prints the raw numerical maze
+    public void printRawMaze() {
+        for (int y = 0; y < dimension; y++) {
+            for (int x = 0; x < dimension; x++) {
+                System.out.print(maze[y][x] + " ");
+            }
+            System.out.println();
+        }
     }
 
-    private Boolean pointNotCorner(Position position, int x, int y) {
-        return (x == position.x || y == position.y);
-    }
-
-    private Boolean pointNotNode(Position position, int x, int y) {
-        return !(x == position.x && y == position.y);
-    }
-
-    public int[][] getMaze(){
-        return maze;
-    }
-    public int getDimension(){
-        return dimension;
-    }
-
-    public void changeMaze(int [][] maze){
-        this.maze = maze;
+    // Prints a symbolic representation of the maze
+    public void printSymbolicMaze() {
+        for (int y = 0; y < dimension; y++) {
+            for (int x = 0; x < dimension; x++) {
+                    System.out.print(maze[y][x] == 0 ? "   " : " " + maze[y][x] + " "); // Path or wall
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,19 +1,8 @@
 public class Position {
-    public int x;
-    public int y;
+    int x, y; // x for column, y for row
 
     Position(int x, int y) {
         this.x = x;
         this.y = y;
     }
-
-    public Position(Position another){
-        this(another.x, another.y);
-    }
-
-    public void changePosition(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-
 }
