40fa54c509666201eeea088d54f0ad8eaad110b8
Andy Hui <chhuiwork@gmail.com>
2023-11-23 00:11:27 +0800

    Project-updated

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,7 @@
 public class main {
     static int [][] array;
     static MazeGenerator gameMaze = new MazeGenerator(30);
-
+    //global game window
     static public Window gameWindow;
     public static void main(String[] args) {
         //Create the starting menu
